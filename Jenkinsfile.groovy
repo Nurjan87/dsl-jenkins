bc9813601c024fc1bfbd5efd44619ceea77cfe05
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 15:17:21 -0500

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,13 +17,13 @@ pipeline{
             steps{
                 ws("tmp/"){
                     script {
-                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        def exists = fileExists 'terraform_0.11.13_linux_amd64.zip'
                         if (exists) {
-                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.11.13_linux_amd64.zip"
                             sh "mv terraform /bin"
                             sh "terraform version"
                         } else {
-                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
                             sh "mv terraform /bin"
                         }
                     }
