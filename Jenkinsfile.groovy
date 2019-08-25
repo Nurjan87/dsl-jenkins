25e160692909f2a12169f8efdde4af2b2815456b
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 15:20:26 -0500

    installin

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,35 @@
 pipeline{
-    agent any 
+    agent any
     stages{
         stage("Run Command"){
             steps{
                 sh '''
                 set +xe
                 echo Hello
-                ech Error
-                sudo yum install httpd -y 
+                ech  Error
                 sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
                 '''
             }
         }
-       stage("Download Terraform"){
+        stage("Download Terraform"){
             steps{
                 ws("tmp/"){
                     script {
-                        def exists = fileExists 'terraform_0.11.13_linux_amd64.zip'
+                      def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
-                            sh "unzip -o terraform_0.11.13_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
                             sh "terraform version"
                         } else {
-                            sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
-                            sh "mv terraform /bin"
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
                         }
                     }
                 }
             }
         }
+    }
+}
