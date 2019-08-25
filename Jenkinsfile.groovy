3382366e9b87a8746a5248f68573fdf506e8c9c7
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 15:42:02 -0500

    install packer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,7 @@ pipeline{
             steps{
                 ws("tmp/"){
                     script {
-                      def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
                             sh "sudo mv terraform /bin"
@@ -33,15 +33,17 @@ pipeline{
         }
         stage("Write to a file"){
             steps{
-                ws("tmp/"){ 
+                ws("tmp/"){
                     writeFile text: "Test", file: "TestFile"
+                    sh "cat TestFile"
                 }
             }
-             stage("Download Packer"){
-             steps{
+        }
+        stage("Download Packer"){
+            steps{
                 ws("tmp/"){
                     script {
-                      def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
+                        def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o packer_1.4.3_linux_amd64.zip"
                             sh "sudo mv packer /bin"
@@ -56,6 +58,3 @@ pipeline{
                 }
             }
         }
-    }
-}
-}
