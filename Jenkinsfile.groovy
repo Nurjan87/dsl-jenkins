ddba5ef9b3595522ef0ad35de651514f4f8dde4c
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 15:35:42 -0500

    installing packer

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -37,6 +37,25 @@ pipeline{
                     writeFile text: "Test", file: "TestFile"
                 }
             }
+             stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                      def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        } else {
+                            sh "wget https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip"
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
         }
     }
 }
