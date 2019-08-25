887748acdeb307506104a8b9e533af364bf4ad24
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 14:59:17 -0500

    unzip

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,6 +8,7 @@ pipeline{
                 echo Hello
                 ech Error
                 sudo yum install httpd -y 
+                sudo yum install httpd wget unzip -y
                 ping -c 4 google.com
                 '''
             }
@@ -17,6 +18,7 @@ pipeline{
                 ws("tmp/"){
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
+                    sh "unzip terraform_o.11.13_linux_amd64.zip"
                 }
             }
         }
