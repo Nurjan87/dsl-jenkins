920b9151de5bc863a3d2635c5dd210aa479d74ca
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-25 14:59:59 -0500

    added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,7 +18,7 @@ pipeline{
                 ws("tmp/"){
                     sh "pwd"
                     sh "wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip"
-                    sh "unzip terraform_o.11.13_linux_amd64.zip"
+                    sh "unzip terraform_0.11.13_linux_amd64.zip"
                 }
             }
         }
